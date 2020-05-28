bfdd38e1952a9662a989b3facbaf87629f9f9fb3
crn1 <gluvajic@aol.com>
2020-05-28 17:48:15 +0200

    Finished basic program

--- sivonja.py
+++ sivonja.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+from datetime import date
 os.system('cls')
 
 def pokreni_komandu(komanda):
@@ -12,11 +13,18 @@ print('=== Sivonja ===\n')
 print('Sivonja kompresuje jezgro...')
 pokreni_komandu('tar --force-local --exclude-vcs --overwrite -C C:/laragon/www/wordpress/wp-content/plugins/ -cf C:/laragon/www/wordpress/wp-content/themes/planeta/inc/tgm/plugins/planeta-core.zip planeta-core')
 
-for i in range(1, 3):
-    print('Sivonja kopira Planetu u wordpress-demo-' + str(i) + '...')
-    pokreni_komandu('cp -ruv C:/laragon/www/wordpress/wp-content/themes/planeta/* C:/laragon/www/wordpress-demo-' + str(i) + '/wp-content/themes/planeta/')
-
 print('Sivonja kompresuje Planetu...')
 pokreni_komandu('tar --force-local --exclude-vcs --overwrite -C C:/laragon/www/wordpress/wp-content/themes/planeta -cf planeta.zip .')
+
+print('Sivonja pravi bekap...')
+danas = date.today()
+pokreni_komandu('mkdir backup')
+pokreni_komandu('tar --force-local --exclude-vcs --overwrite -C C:/laragon/www/wordpress/wp-content/themes/planeta -cf backup/planeta-' + str(danas) + '.zip .')
+
+for i in range(1, 3):
+    print('Sivonja kopira jezgro u wordpress-demo-' + str(i) + '...')
+    pokreni_komandu('cp -ruv C:/laragon/www/wordpress/wp-content/plugins/planeta-core/* C:/laragon/www/wordpress-demo-' + str(i) + '/wp-content/plugins/planeta-core/')
+    print('Sivonja kopira Planetu u wordpress-demo-' + str(i) + '...')
+    pokreni_komandu('cp -ruv C:/laragon/www/wordpress/wp-content/themes/planeta/* C:/laragon/www/wordpress-demo-' + str(i) + '/wp-content/themes/planeta/')
 
 print('Sivonja zavrsio!')
