b4dc003d8c9dec042804752ce3d13222a030fdfb
crn1 <gluvajic@aol.com>
2020-05-31 11:35:56 +0200

    Replaced tar with zip

--- sivonja.py
+++ sivonja.py
@@ -13,20 +13,12 @@ def pokreni_komandu(komanda):
 print('=== Sivonja ===\n')
 
 print('Sivonja kompresuje jezgro...')
-pokreni_komandu('tar --force-local --exclude-vcs --overwrite -C C:/laragon/www/wordpress/wp-content/plugins/ -cf C:/laragon/www/wordpress/wp-content/themes/planeta/inc/tgm/plugins/planeta-core.zip planeta-core')
+pokreni_komandu('cd C:/laragon/www/wordpress/wp-content/plugins/ && zip -rq9 ../themes/planeta/inc/tgm/plugins/planeta-core planeta-core/* -x *.git*')
 
 print('Sivonja kompresuje Planetu...')
-pokreni_komandu('tar --force-local --exclude-vcs --overwrite -C C:/laragon/www/wordpress/wp-content/themes/planeta -cf planeta.zip .')
+pokreni_komandu('cd C:/laragon/www/wordpress/wp-content/themes/planeta/ && zip -rq9 %USERPROFILE%/Desktop/planeta_backup/planeta' + str(danas) + ' . -x *.git*')
 
-print('Sivonja pravi bekap jezgra...')
-pokreni_komandu('mkdir planeta-core-backup')
-pokreni_komandu('tar --force-local --exclude-vcs --overwrite -C C:/laragon/www/wordpress/wp-content/plugins/ -cf planeta-core-backup/planeta-core-' + str(danas) + '.zip planeta-core')
-
-print('Sivonja pravi bekap Planete...')
-pokreni_komandu('mkdir planeta-backup')
-pokreni_komandu('tar --force-local --exclude-vcs --overwrite -C C:/laragon/www/wordpress/wp-content/themes/planeta -cf planeta-backup/planeta-' + str(danas) + '.zip .')
-
-for i in range(1, 3):
+for i in range(1, 4):
     print('Sivonja kopira jezgro u wordpress-demo-' + str(i) + '...')
     pokreni_komandu('cp -ruv C:/laragon/www/wordpress/wp-content/plugins/planeta-core/* C:/laragon/www/wordpress-demo-' + str(i) + '/wp-content/plugins/planeta-core/')
     print('Sivonja kopira Planetu u wordpress-demo-' + str(i) + '...')
